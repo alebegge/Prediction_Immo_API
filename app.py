3bff739685444feb0272e3c79aa3a92d51e97d6b
Alexandre Le Begge <alexandre.lebegge@gmail.com>
2022-03-30 17:01:18 +0200

    add new features, doc, prepross

--- app.py
+++ app.py
@@ -15,15 +15,43 @@ def predicted():
     error = None
     input_json = request.get_json(force=True)
     if request.method == 'POST':
-        input_json = preprocess(input_json)
-        if input_json:
-            return predict(input_json)
+        data_prep = preprocess(input_json["data"])
+        if data_prep:
+            return predict(data_prep)
         else:
             error = 'Invalid data, please refer to the documentation.'
             return error
+    if request.method == 'GET':
+        return doc
     else:
         error = 'This route only takes POST as method for now.'
         return error
+
+
+doc ="""
+{
+  "data": 
+  {
+    "area": int,
+    "property-type": "APARTMENT" | "HOUSE" | "OTHERS",
+    "rooms-number": int,
+    "zip-code": int,
+    "land-area": Optional[int],
+    "garden": Optional[bool],
+    "garden-area": Optional[int],
+    "equipped-kitchen": Optional[bool],
+    "full-address": Optional[str],
+    "swimming-pool": Optional[bool],
+    "furnished": Optional[bool],
+    "open-fire": Optional[bool],
+    "terrace": Optional[bool],
+    "terrace-area": Optional[int],
+    "facades-number": Optional[int],
+    "building-state": Optional[
+      "NEW" | "GOOD" | "TO RENOVATE" | "JUST RENOVATED" | "TO REBUILD"
+    ]
+  }
+} """
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
--- predict/prediction.py
+++ predict/prediction.py
@@ -1,11 +1,54 @@
 import json
+import pandas as pd
+import numpy as np
+import sys
+# from model.price_pred_model import features
 
+features = [
+    "Livable surface",
+    "Type property",
+    "Brussels",
+    "Walloon Brabant",
+    "Flemish Brabant",
+    "Antwerp",
+    "Limburg",
+    "Liege",
+    "Namur",
+    "Luxembourg",
+    "Hainaut",
+    "West Flanders",
+    "East Flanders",
+    "Kitchen equipment",
+    "State of the property",
+    "Furnished",
+    "Number of facades",
+    "Surface terrace",
+    "Surface garden"
+]
+json_out =  {
+        "East Flanders": 1,
+        "Furnished": 0,
+        "Kitchen equipment": 2,
+        "Livable surface": 150,
+        "Number of facades": 2,
+        "State of the property": 1,
+        "Surface garden": 0,
+        "Surface terrace": 0,
+        "Type property": 1
+    }
+
+df_test = pd.DataFrame(columns=features)
+df_test = df_test.append(json_out,ignore_index=True)
+df_test = df_test.fillna(0)
 
 def predict(json_input) -> json:
     """
     Return a predict price based on json input by the user.
     """
-    json_output = {}
-    json_output["data"] = json_input
-    return json_output
-
+    # json_output = {}
+    # json_output["data"] = json_input
+    # return json_output
+    df_test = pd.DataFrame(columns=features)
+    df_test = df_test.append(json_out,ignore_index=True)
+    df_test = df_test.fillna(0)
+    return df_test.to_dict('split')
